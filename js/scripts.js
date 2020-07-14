1189379cbcee71d2629370296dcc490e1964ae80
joreiyo <62222850+joreiyo@users.noreply.github.com>
2020-07-13 21:25:53 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,11 @@ const getTokenParameter = () => {
   const token = urlParams.get('code');
   console.log(token);
   window.sessionStorage.setItem('Token', token);
+  
+  fetch(
+  'https://login.questrade.com/oauth2/token?client_id=q2bPAJ7jLyRaj71swF4Ep8cHc4wAlQ&code=7AvCOewbnRs7VaiyCL-YAOm_gkEHuhq20&grant_type=authorization_code&redirect_uri=https://joreiyo-org.github.io/qtrade/callback.html',
+  { method: 'post' }
+)
+  .then((response) => response.json())
+  .then((data) => window.sessionStorage.setItem('AccesToken', token));
 };
