46f5fed31b226a36a75157086f5ab36848f004e6
joreiyo <62222850+joreiyo@users.noreply.github.com>
2020-07-15 23:34:33 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ const getTokenParameter = () => {
   const oauthEndPoint = 'https://login.questrade.com/oauth2/token?client_id=q2bPAJ7jLyRaj71swF4Ep8cHc4wAlQ&code=' + oauthCode + '&grant_type=authorization_code&redirect_uri=https://joreiyo-org.github.io/qtrade/callback.html';
   
   fetch(oauthEndPoint,
-	{method: 'POST', mode: 'no-cors',
+	{method: 'POST',
 		headers: {'Access-Control-Allow-Origin' : 'https://joreiyo-org.github.io'} 
 	}
 )
