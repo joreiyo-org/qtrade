6834f9a150d399e2e64d7597c8acdef06c3fb899
joreiyo <62222850+joreiyo@users.noreply.github.com>
2020-07-15 23:18:44 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,7 @@ const getTokenParameter = () => {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const oauthCode = urlParams.get('code');
-  console.log(token);
-//  window.sessionStorage.setItem('Token', oauthCode);
-    const oauthEndPoint = 'https://login.questrade.com/oauth2/token?client_id=q2bPAJ7jLyRaj71swF4Ep8cHc4wAlQ&code=' + oauthCode + '&grant_type=authorization_code&redirect_uri=https://joreiyo-org.github.io/qtrade/callback.html';
+  const oauthEndPoint = 'https://login.questrade.com/oauth2/token?client_id=q2bPAJ7jLyRaj71swF4Ep8cHc4wAlQ&code=' + oauthCode + '&grant_type=authorization_code&redirect_uri=https://joreiyo-org.github.io/qtrade/callback.html';
   
     fetch(oauthEndPoint,
 	{method: 'POST', 
